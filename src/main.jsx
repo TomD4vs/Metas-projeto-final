fd6654a83c4fb16e7967ef3e88103f761bad00a1
TomD4vs <elyton.moreira163@gmail.com>
2026-03-20 21:53:44 -0400

    test

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,14 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import { MetasProvider } from './contexts/MetasContext'
 import './index.css'
 import App from './App.jsx'
-
+ 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <BrowserRouter basename="/metas-projeto-final/">
-      <MetasProvider>
-        <App />
-      </MetasProvider>
-    </BrowserRouter>
-  </StrictMode>
+  <BrowserRouter basename="/Metas-projeto-final/">
+    <MetasProvider>
+      <App />
+    </MetasProvider>
+  </BrowserRouter>
 )
+ 
--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/metas-projeto-final/'
+  base: '/Metas-projeto-final/'
 })
