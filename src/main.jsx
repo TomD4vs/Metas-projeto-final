454e3f654de36c223b934e6df13b086b21909c31
TomD4vs <elyton.moreira163@gmail.com>
2026-03-20 21:39:55 -0400

    fix redirecionamento

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter basename="/Metas-projeto-final/">
+    <BrowserRouter basename="/metas-projeto-final/">
       <MetasProvider>
         <App />
       </MetasProvider>
